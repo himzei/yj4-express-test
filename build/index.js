52a4fb73718b1d694ace0881748f32fa20287ae0
조현일 <himzei@gmail.com>
2024-02-03 23:33:40 +0900

    babel

---
+++ build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var app = (0, _express["default"])();
+app.get("/", function (req, res) {
+  res.send({
+    name: "hello how are you? "
+  });
+});
+app.listen("4000", function () {
+  return console.log("server on http://localhost:4000");
+});
