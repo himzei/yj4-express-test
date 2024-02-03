ada0cf6d2424c1519a0bf6ffbcfe35cd3262fe67
조현일 <himzei@gmail.com>
2024-02-03 22:31:33 +0900

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ name: "hello how are you? " });
+});
+
+app.listen("4000", () => console.log(`server on http://localhost:4000`));
