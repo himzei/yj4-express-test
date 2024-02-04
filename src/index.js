3dd63582e867ad8a2321a0b7296d86f27dc9045a
조현일 <himzei@gmail.com>
2024-02-04 21:10:23 +0900

    re

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ name: "hello how are you? and you" });
+  res.send({ name: "hello" });
 });
 
 app.listen("4000", () => console.log(`server on http://localhost:4000`));
