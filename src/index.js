6bab28eccdf6a589bbf29f1779e33985cdf477ea
조현일 <himzei@gmail.com>
2024-02-04 21:54:51 +0900

    npm build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ name: "hello!! 안녕" });
+  res.send({ name: "hello!! 안녕 안녕하세요 " });
 });
 
 app.listen("4000", () => console.log(`server on http://localhost:4000`));
