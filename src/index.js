321f574fc92de47a1e0015e124588d671eebc8ec
조현일 <himzei@gmail.com>
2024-02-04 22:31:25 +0900

    re

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ name: "hello!! 안녕 안녕하세요 조현일입니다 " });
+  res.send({ name: "hello!! 안녕 안녕하세요 조현일입니다 dsalfjasdf " });
 });
 
 app.listen("4000", () => console.log(`server on http://localhost:4000`));
