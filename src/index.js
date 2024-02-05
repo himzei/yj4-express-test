44befc49bb821eed7d5f7dbccb34074139cf3421
조현일 <himzei@gmail.com>
2024-02-05 16:45:37 +0900

    3000 port

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
   res.send({ name: "hello!! 안녕 안녕하세요 조현일입니다 반가워요" });
 });
 
-app.listen("4000", () => console.log(`server on http://localhost:4000`));
+app.listen("3000", () => console.log(`server on http://localhost:3000`));
